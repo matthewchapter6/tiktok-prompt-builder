968cd3664ea5dc5823e9ca459739959a7e21fbb2
matthewchapter6 <matthewchapter6@gmail.com>
2026-03-24 14:32:13 +0800

    Create init.js

---
+++ src/constants/init.js
@@ -0,0 +1,14 @@
+export const init = {
+  platform: "tiktok", grokPlan: "free", totalDuration: "12",
+  funnel: "",
+  productName: "", productCategory: "", keyColors: "", keyFeatures: "", usp: "", productRules: "",
+  videoStyle: "ugc", tone: [],
+  settingPreset: "", settingCustom: "", settingDetail: "",
+  lightingPreset: "", lightingCustom: "",
+  talent: "solo_female", talentStyle: [], talentDetail: "",
+  cameraMove: "gentle_handheld", heroAngle: "",
+  hook: [], problemStatement: "", keyBenefit: "", cta: [],
+  voLang: "", voTone: "", customVO: "",
+  restrictions: [], extraNotes: "",
+  storyline: "",
+};
